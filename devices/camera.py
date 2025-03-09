41f1c12b30f41d885ca5838da89e226123aa9f8f
ArunanElamaran <arunan.elamaran@gmail.com>
2025-03-09 11:14:19 -0700

    Removed Unnecessary Code

--- devices/camera.py
+++ devices/camera.py
@@ -38,26 +38,6 @@ class LidarCamera:
         finally:
             pipeline.stop()
 
-    def get_color_matrix(self):
-        pipeline =self.rs.pipeline()
-        pipeline.start()
-
-        try:
-            while True:
-            # Create a pipeline object. This object configures the streaming camera and owns it's handle
-                for i in range(3):
-                	frames = pipeline.wait_for_frames()
-                frames = pipeline.wait_for_frames()
-                color = frames.get_color_frame()
-                if not color:
-                    continue
-                color_data = color.as_frame().get_data()
-                np_image = self.np.asanyarray(color_data)
-                return np_image
-                break
-        finally:
-            pipeline.stop()
-
     def get_depth_image(self):
         image = self.Image.fromarray(self.get_depth_matrix())
         image.save(self.depth_image_path)
